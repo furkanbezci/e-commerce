c2af2df52b97165fafbff1ce6fd9a39705755092
furkanbezci <baburfurkan1@gmail.com>
2025-09-15 02:15:37 +0300

    suspense added

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,16 @@
 import { Header } from "@/components/Header";
 import Footer from "@/components/Footer";
+import { Suspense } from "react";
 
 export default function Layout({ children, showHeader = true, showFooter = true }) {
   return (
     <div className="min-h-screen bg-gray-100">
       {showHeader && <Header />}
-      <main>{children}</main>
+      <main>
+      <Suspense fallback={<div>Yükleniyor...</div>}>
+          {children}
+        </Suspense>
+      </main>
       {showFooter && <Footer />}
     </div>
   );
